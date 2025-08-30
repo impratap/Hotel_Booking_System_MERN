e9177ddd2343f39df2fed7bbcbb9c6966b544030
impratap <ankitpratapsingh333@gmail.com>
2025-08-31 01:19:22 +0530

    Stripe Payment added

--- server/controllers/stripeWebhooks.js
+++ server/controllers/stripeWebhooks.js
@@ -1,4 +1,4 @@
-import stripe from 'stripe';
+import Stripe from 'stripe';
 import Booking from '../models/Booking.js'
 
 
@@ -7,14 +7,14 @@ import Booking from '../models/Booking.js'
 
 export const stripeWebhooks = async (request, response) => {
     // Stripe Gateway Intialize 
-    const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY)
+    const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY)
     const sig = request.headers['stripe-signature'];
     let event;
 
     try {
         event = stripeInstance.webhooks.constructEvent(request.body, sig, process.env.STRIPE_WEBHOOK_SECRET)
     } catch (error) {
-        response.status(400).send(`Webhook Error: ${error.message}`)
+        return response.status(400).send(`Webhook Error: ${error.message}`)
     }
 
     // Handle the event
